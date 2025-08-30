afc041930d9ecdad6634e16284aacd7f151df9c1
Bipin Thapa <yukichin638@gmail.com>
2025-08-30 22:06:42 +0545

    right nav created

--- src/app/newsFeed.js
+++ src/app/newsFeed.js
@@ -2,6 +2,6 @@ import RightNav from "./rightNav"
 
 export default function NewsFeed({feedData}){
   return <div id="homepage-right-container">
-    <RightNav user={feedData.user}/>
+    <RightNav feedData={feedData}/>
   </div>
 }
--- src/app/rightNav.js
+++ src/app/rightNav.js
@@ -3,15 +3,21 @@ import { FaUserPlus } from "react-icons/fa";
 import { FaRegBell } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { TbMessage } from "react-icons/tb";
+import { IoMdSearch } from "react-icons/io";
 
-export default function RightNav({ user }) {
+export default function RightNav({ feedData }) {
+  const imgurl = feedData.user ? feedData.user.imgurl : '/user.jpg';
   return <div id="homepage-right-nav">
-    <div>
-      <FaUserPlus />
-      <FaRegBell />
-      <MdOutlineEmail />
-      <TbMessage />
-      <Image src={user.imgurl} alt="logo" width={100} height={100}/>
+    <div id="right-nav-search-container">
+      <IoMdSearch id="right-nav-search-icon"/>
+      <input type="text" placeholder="search EDUCTION" autoComplete="off" id="right-nav-search"/>
+    </div>
+    <div id="right-nav-options">
+      <div className="right-nav-opt"><FaUserPlus /></div>
+      <div className="right-nav-opt"><FaRegBell /></div>
+      <div className="right-nav-opt"><MdOutlineEmail /></div>
+      <div className="right-nav-opt"><TbMessage /></div>
+      <Image src={imgurl} alt="logo" width={100} height={100} id="right-nav-image"/>
     </div>
   </div>
 }

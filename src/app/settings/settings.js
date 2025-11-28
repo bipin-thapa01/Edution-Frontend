6356e3f447ba0110191dcce35edbfb44bf963c84
Bipin Thapa <yukichin638@gmail.com>
2025-11-28 14:14:15 +0545

    settings page

--- src/app/settings/settings.js
+++ src/app/settings/settings.js
@@ -1,138 +1,166 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import PostImage from "../postImage/postImage";
-import { Ring } from 'ldrs/react';
-import 'ldrs/react/Ring.css';
+import { Ring } from "ldrs/react";
+import "ldrs/react/Ring.css";
 
-export default function Settings({fetchData}) {
-  const [rightContent, setRightContent] = useState(null);
+export default function Settings({ fetchData }) {
+  const textData = useRef();
+  const textAreaData = useRef();
+  const password = useRef();
+  const type = ['name', 'username', 'pfp', 'bio', 'bg'];
+  const [selectedIndex, setSelectedIndex] = useState(null);
   const [inputPlaceholder, setInputPlaceholder] = useState("Loading....");
   const [imageList, setImageList] = useState([]);
-  const data = [{
-    title: 'Change Name',
-    description: 'Enter your new name',
-  },
-  {
-    title: 'Change Username',
-    description: 'Enter your new username',
-  },
-  {
-    title: 'Change Profile Picture',
-    description: 'Upload new profile picture',
-  },
-  {
-    title: 'Change Bio',
-    description: 'Enter your new bio',
-  },
-  {
-    title: 'Change Background Image',
-    description: 'Upload new background image',
-  },
+
+  const data = [
+    { title: "Change Name", description: "Enter your new name" },
+    { title: "Change Username", description: "Enter your new username" },
+    { title: "Change Profile Picture", description: "Upload new profile picture" },
+    { title: "Change Bio", description: "Enter your new bio" },
+    { title: "Change Background Image", description: "Upload new background image" },
   ];
-
-  const uploadPost = async () => {
-    const postText = document.getElementById('post-text').value;
-    if (imageList.length === 0 && postText === '') {
-      alert('Either photo or text is required!');
-      return;
-    }
-    const formData = new FormData();
-    formData.append('file', imageList[0]);
-    formData.append('upload_preset', 'edution_users_upload');
-    const imageRes = await fetch(`https://api.cloudinary.com/v1_1/dlpm6yyad/image/upload`, {
-      method: 'POST',
-      body: formData,
-    })
-    const imageData = await imageRes.json();
-
-    const res = await fetch('http://localhost:8080/api/post', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        by: loginData.username,
-        description: postText,
-        imgurl: imageData.secure_url,
-      })
-    })
-    const data = res.text();
-    console.log(data);
-    setImageList([]);
-    document.getElementById('post-text').value = '';
-  }
 
   const selectImage = (e) => {
     if (!e.target.files || e.target.files.length === 0) return;
     const file = e.target.files[0];
     setImageList([file]);
-    e.target.value = null;
-    console.log(imageList)
   };
 
-  useEffect(()=>{
-    if(fetchData?.user !== null){
-      console.log(fetchData)
-      setInputPlaceholder(fetchData?.user?.bio);
-      setRightContent(
-        <>
-          <div id="settings-right-container-header">
-            {data[0].title}: 
-          </div>
-        </>
-      );
-      document.getElementById(`settings-left-container-option0`).classList.add('selected-option');
+  useEffect(() => {
+    if (fetchData?.user) {
+      setInputPlaceholder(fetchData.user.bio);
+      setSelectedIndex(0);
     }
-  },[fetchData]);
+  }, [fetchData]);
 
-  const displayRightContent = (index) => {
-    setRightContent(
+  const renderRightContent = () => {
+    if (selectedIndex === null) {
+      return (
+        <div id="settings-loader">
+          <Ring color="#6614b8" size={30} speed={2} bgOpacity={0.2} />
+        </div>
+      );
+    }
+
+    const item = data[selectedIndex];
+
+    const updateData = async () => {
+      const passwordData = password.current?.value;
+      if (selectedIndex === 0 || selectedIndex === 1 || selectedIndex === 3) {
+        const updateData = selectedIndex !== 3 ?  textData.current?.value : textAreaData.current?.value;
+        const res = await fetch('http://localhost:8080/api/update-text', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: fetchData?.user?.email,
+            updateData: updateData,
+            type: type[selectedIndex],
+            password: passwordData,
+          })
+        });
+        const data = await res.json();
+        console.log(data);
+      }
+      else {
+        const formData = new FormData();
+        formData.append('file', imageList[0]);
+        formData.append('upload_preset', 'edution_users_upload');
+        const imageRes = await fetch(`https://api.cloudinary.com/v1_1/dlpm6yyad/image/upload`, {
+          method: 'POST',
+          body: formData,
+        })
+        const imageData = await imageRes.json();
+        const updateData = imageData.secure_url;
+        const res = await fetch('http://localhost:8080/api/update-text', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: fetchData?.user?.email,
+            updateData: updateData,
+            type: type[selectedIndex],
+            password: passwordData,
+          })
+        });
+      }
+    }
+
+    return (
       <>
-        <div id="settings-right-container-header">
-          {data[index].title}
-        </div>
+        <div id="settings-right-container-header">{item.title}</div>
+
         <div id="settings-right-container-change">
-          <div>{data[index].description}</div>
-          {
-            index === 2 || index === 4 ? <input type="file" accept="image/*" onChange={selectImage}/> :  index === 3 ? <textarea id="settings-right-container-textarea" defaultValue={inputPlaceholder}/> : <input id="settings-right-container-input" type="text" autoComplete="off" placeholder={data[index].description}/>
-          }
+          <div>{item.description}</div>
+
+          {selectedIndex === 2 || selectedIndex === 4 ? (
+            <input type="file" accept="image/*" onChange={selectImage} />
+          ) : selectedIndex === 3 ? (
+            <textarea
+              id="settings-right-container-textarea"
+              defaultValue={inputPlaceholder}
+              ref={textAreaData}
+            />
+          ) : (
+            <input
+              id="settings-right-container-input"
+              type="text"
+              autoComplete="off"
+              placeholder={item.description}
+              ref={textData}
+            />
+          )}
         </div>
+
         <div id="settings-right-container-password">
-          <div>Enter your current password: </div>
-          <input id="settings-right-container-password-input" type="password" autoComplete="off" placeholder="Enter your current password"/>
+          <div>Enter your current password:</div>
+          <input
+            id="settings-right-container-password-input"
+            type="password"
+            autoComplete="off"
+            placeholder="Enter your current password"
+            ref={password}
+          />
         </div>
-        <PostImage imageList={imageList}/>
+
+        {(selectedIndex === 2 || selectedIndex === 4) && (
+          <PostImage imageList={imageList} />
+        )}
+
+        <button id="settings-page-server-update-button" onClick={updateData}>Update</button>
       </>
     );
-    document.querySelectorAll('.settings-left-container-option').forEach(option => {
-      option.classList.remove('selected-option');
-    });
-    document.getElementById(`settings-left-container-option${index}`).classList.add('selected-option');
-  }
+  };
 
-  return <div id="settings-container">
-    <div id="settings-left-container">
-      <div id="settings-left-container-header">
-        Settings
-      </div>
-      <div id="settings-left-container-options">
-        {
-          data.map((item, index) => {
-            return <div onClick={() => displayRightContent(index)} key={index} className="settings-left-container-option" id={`settings-left-container-option${index}`}>
+  return (
+    <div id="settings-container">
+      <div id="settings-left-container">
+        <div id="settings-left-container-header">Settings</div>
+
+        <div id="settings-left-container-options">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className={`settings-left-container-option ${selectedIndex === index ? "selected-option" : ""
+                }`}
+              onClick={() => {
+                setSelectedIndex(index);
+                setImageList([]);
+              }}
+            >
               <div>{item.title}</div>
               <div>
                 <MdKeyboardArrowRight />
               </div>
             </div>
-          })
-        }
+          ))}
+        </div>
       </div>
+
+      <div id="settings-right-container">{renderRightContent()}</div>
     </div>
-    <div id="settings-right-container">
-      {rightContent ? rightContent :
-        <div id="settings-loader">
-          <Ring color="#6614b8" size={30} speed={2} bgOpacity={0.2} />
-        </div>}
-    </div>
-  </div>
+  );
 }
